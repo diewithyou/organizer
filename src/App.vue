3d4d9115309126cce6d43a21411247abfc2adedc
death <mail@example.com>
2017-09-11 00:43:31 +0200

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <v-app toolbar>
+    <NavigationDrawer :drawer="drawer"></NavigationDrawer>
+    <v-toolbar fixed class="deep-orange" dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <!--v-router-->
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
+import NavigationDrawer from './components/NavigationDrawer/NavigationDrawer'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    NavigationDrawer
+  },
+  data () {
+    return {
+      drawer: null
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavigationDrawer/NavigationDrawer.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+    temporary
+    v-model="drawerVisible"
+    light
+    overflow
+    absolute
+  >
+    <v-list class="pa-1">
+      <v-list-tile avatar>
+        <v-list-tile-avatar>
+          <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>John Leider</v-list-tile-title>
+        </v-list-tile-content>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+      <v-list-tile v-for="item in items" :key="item.title" @click="">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ['drawer'],
+  computed: {
+    drawerVisible: {
+      get () {
+        return this.drawer
+      },
+      set (value) {
+        this.darwer = value
+      }
+    }
+  },
+  mounted () {
+    console.log('dupa', this)
+    console.log('xxx', this.drawer)
+
+    setInterval(() => {
+      console.log('sss', this.drawer)
+      console.log('aaa', this.drawerVisible)
+    }, 1000)
+  },
+  data () {
+    return {
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ]
+    }
+  }
+}
+</script>
