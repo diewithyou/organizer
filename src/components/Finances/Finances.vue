5df53dc053a79f1545dd9babbd0db365b42208a9
death <mail@example.com>
2017-11-20 22:06:16 +0100

    finances, add underscore and momentjs

--- src/components/Finances/Finances.vue
+++ src/components/Finances/Finances.vue
@@ -2,7 +2,7 @@
   <v-flex xs12 sm6 offset-sm3>
     <v-data-table
       v-bind:headers="headers"
-      :items="data"
+      :items="statistic"
       hide-actions
       class="elevation-1"
     >
@@ -16,21 +16,55 @@
 </template>
 
 <script>
+  import {mapGetters} from 'vuex'
+  import moment from 'moment'
+  import _ from 'underscore'
+
   export default {
+    computed: {
+      ...mapGetters([
+        'getTasks'
+      ]),
+      statistic () {
+        let data = []
+
+        for (let i = 0; i < 12; i++) {
+          data.push({
+            month: moment().month(i).format('MMMM'),
+            income: 0,
+            tasks: 0
+          })
+        }
+
+        _.each(this.getTasks, function (item) {
+          let month = parseInt(moment(item.start).format('M'))
+          data[month].income += item.price
+          data[month].tasks++
+        })
+
+        return data
+      }
+    },
+    mounted () {
+      console.log('statistic', this.statistic)
+    },
     data () {
       return {
         headers: [
           {
             text: 'Month',
-            value: 'month'
+            value: 'month',
+            sortable: false
           },
           {
             text: 'Tasks',
-            value: 'tasks'
+            value: 'tasks',
+            sortable: false
           },
           {
             text: 'Income',
-            value: 'income'
+            value: 'income',
+            sortable: false
           }
         ],
         data: [
--- src/store/tasks/index.js
+++ src/store/tasks/index.js
@@ -5,16 +5,19 @@ export default {
     tasks: [
       {
         title: 'All Day Event',
-        start: '2017-10-30T19:00:00'
+        start: '2017-10-30T19:00:00',
+        price: 76
       },
       {
         title: 'Long Event',
-        start: '2017-10-30T08:35:00'
+        start: '2017-10-30T08:35:00',
+        price: 40
       },
       {
         id: 999,
         title: 'Repeating Event',
-        start: '2017-10-30T16:00:00'
+        start: '2017-10-30T16:00:00',
+        price: 30.3
       }
     ],
     newTask: {}
