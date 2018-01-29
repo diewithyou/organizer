531c55ec56d6b6a2b9299cbfed48ce8964b176c4
Konrad Świć <kmswic@gmail.com>
2018-01-29 13:22:26 +0100

    Adding tasks to calendar

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -5,8 +5,9 @@
     <v-btn class="blue--text darken-1" flat @mousedown.native="show">Dupka</v-btn>
     <full-calendar
       :editable="true"
-      :config="config"
-      @day-click="dayClick"
+      day-click="dayClick"
+      @event-created="eventCreated"
+      :events="getTasks"
     > </full-calendar>
   </div>
 </template>
@@ -18,7 +19,7 @@
   // import {fullcalendar} from 'fullcalendar';
   import {FullCalendar} from 'vue-full-calendar';
   import AddTaskModal from './AddTaskModal';
-import {OPEN_DIALOG} from '../../store/mutation-types';
+  import {OPEN_DIALOG, ADD_NEW_TASK} from '../../store/mutation-types';
 
   export default {
     components: {
@@ -28,62 +29,34 @@ import {OPEN_DIALOG} from '../../store/mutation-types';
     computed: {
       ...mapGetters([
         'getTasks'
-      ]),
-      config () {
-        return {
-          events: this.getTasks
-        };
-      }
+      ])
     },
     mounted () {
-      // console.log('bind', this);
-      // this.cal = jQuery('.calendar').fullCalendar({
-      //   header: {
-      //     left: 'prev,next today',
-      //     center: 'title',
-      //     right: 'month,agendaWeek,agendaDay'
-      //   },
-      //   events: this.getTasks,
-      //   editable: true,
-      //   eventClick: (calEvent, jsEvent, view) => {
-      //     console.log('Event: ' + calEvent.title);
-      //     console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-      //     console.log('View: ' + view.name);
-      //   },
-      //   dayClick: function (date, jsEvent, view) {
-      //     console.log('jsEvent', jsEvent);
-      //     console.log('Clicked on:', date.format(), date, date.toISOString(), date.format('HH:mm:ss'));
-      //     console.log('Coordinates:', jsEvent.pageX, jsEvent.pageY);
-      //     console.log('Current view:', view);
-      //     console.log('this', this);
-
-// //          cal.fullCalendar('renderEvent', {title: 'title', start: date}, true)
-
-          // console.log('ppp', this.cal.fullCalendar('clientEvents'));
-
-          // this.$store.commit(CREATE_NEW_TASK, {
-            // start: date.format()
-          // });
-
-          // to do zmiany w pozniejszym terminie
-          // setTimeout(function () {
-            // this.$store.commit(OPEN_DIALOG);
-          // }.bind(this), 1);
-        // }.bind(this)
-      // });
-    // },
-    // methods: {
-    //   show () {
-    //     this.$store.commit(OPEN_DIALOG);
-    //   },
-    //   addNewTask (newTask) {
-    //     this.$store.commit(ADD_NEW_TASK, newTask);
-    //     this.cal.fullCalendar('renderEvent', newTask);
-    //   }
     },
     methods: {
-      dayClick (date, jsEvent, view) {
+      show () {
         this.$store.commit(OPEN_DIALOG);
+      },
+      addNewTask (newTask) {
+        this.$store.commit(ADD_NEW_TASK, newTask);
+        this.cal.fullCalendar('renderEvent', newTask);
+      },
+      eventCreated (event) {
+        // this.$store.commit(OPEN_DIALOG);
+        // setTimeout(() => {
+        //   this.$store.commit(OPEN_DIALOG);
+        // }, 1);
+        this.$store.commit(ADD_NEW_TASK, {
+          start: event.start.format(),
+          end: event.end.format()
+        });
+      },
+      dayClick (date, jsEvent, view) {
+          // to do zmiany w pozniejszym terminie
+        setTimeout(() => {
+          this.$store.commit(OPEN_DIALOG);
+        }, 1);
+        // }.bind(this)
       }
     }
   };
