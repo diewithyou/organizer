5c026d7b3f36059d710887a7498ebaff085533f2
death <mail@example.com>
2018-01-30 13:41:20 +0100

    edit type of task

---
+++ src/components/TypeOfTasks/DeleteModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <Modal title="Delete Type Of Task">
+    <v-form v-model="valid" slot="body">
+      <div>dupa</div>
+    </v-form>
+    <v-card-actions slot="v-card-actions">
+      <v-spacer></v-spacer>
+      <v-btn class="blue--text darken-1" flat @click.native="closeDialog">Close</v-btn>
+      <v-btn class="blue--text darken-1" flat @click.native="deleteTypeOfTask">Delete</v-btn>
+    </v-card-actions>
+  </Modal>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex';
+  import {CLOSE_DIALOG} from '../../store/mutation-types';
+  import Modal from '../Modal/Modal';
+
+  export default {
+    components: {
+      Modal
+    },
+    props: ['idTypeOfTask'],
+    computed: {
+    },
+    mounted () {
+      // console.log('getNewTask', this.getNewTask);
+      // this.task = this.getTask(this.getEditedTask);
+      // console.log('this task', this.task)
+    },
+    data () {
+      return {
+        valid: false
+      };
+    },
+    methods: {
+
+    }
+  };
+</script>
--- src/store/typeoftasks/mutations.js
+++ src/store/typeoftasks/mutations.js
@@ -1,4 +1,5 @@
 import * as types from '../mutation-types';
+import _ from 'underscore';
 
 export default {
   [types.ADD_TYPE_OF_TASK] (state, payload) {
@@ -6,5 +7,16 @@ export default {
     const id = state.slice(-1)[0].id + 1;
     payload.id = id;
     state.push(payload);
+  },
+
+  [types.EDIT_TYPE_OF_TASK] (state, payload) {
+    console.log('EDIT_TYPE_OF_TASK');
+    _.each(state, function(item) {
+      if (item.id === payload.id) {
+        item.name = payload.name;
+        item.price = payload.price;
+        item.time = payload.time;
+      }
+    });
   }
 };
