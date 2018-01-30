46bb27f11768657a8c33bf078e141e6da48e78b8
death <mail@example.com>
2018-01-30 11:15:12 +0100

    edit type of task

--- src/components/TypeOfTasks/TypeOfTasks.vue
+++ src/components/TypeOfTasks/TypeOfTasks.vue
@@ -12,6 +12,14 @@
         <td class="text-xs-right">{{ props.item.name }}</td>
         <td class="text-xs-right">{{ props.item.time }}</td>
         <td class="text-xs-right">{{ props.item.price.toFixed(2) }}</td>
+        <td class="text-xs-right td-buttons-width">
+          <v-btn outline class="blue--text darken-1 ma-0" flat @click.native="">
+            <v-icon>edit</v-icon>
+          </v-btn>
+          <v-btn outline class="red--text darken-4 ma-0" flat @click.native="openDeleteModal">
+            <v-icon>delete</v-icon>
+          </v-btn>
+        </td>
       </template>
     </v-data-table>
   </v-flex>
@@ -54,7 +62,16 @@
         setTimeout(function () {
           this.$store.commit(OPEN_DIALOG);
         }.bind(this));
+      },
+      openDeleteModal () {
+        console.log('xxx');
       }
     }
   };
 </script>
+
+<style>
+  .td-buttons-width {
+    width: 235px;
+  }
+</style>
